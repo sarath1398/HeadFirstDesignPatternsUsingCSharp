eee2acd0ea94ef259d3a12e869d8d9d2ef557d38
Sarath <sarath.u1398@gmail.com>
2025-12-09 00:53:26 +0530

    Added Proxy pattern changes

---
+++ DesignPatterns/MatchMakingTestDrive/Program.cs
@@ -0,0 +1,53 @@
+﻿using static ProxyPatternDependencies.Classes;
+
+namespace MatchMakingTestDrive
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MatchMakingTestDrive matchMakingTestDrive = new();
+            matchMakingTestDrive.Drive();
+        }
+    }
+
+    public class MatchMakingTestDrive
+    {
+        public void Drive()
+        {
+            IPerson joe = new PersonImpl();
+            joe.SetName("Joe Javabean");
+            joe.SetGeekRating(7);
+
+            //Owner Proxy
+            IPerson ownerProxy = GetOwnerProxy(joe);
+            Console.WriteLine("Name is " + ownerProxy.GetName());
+            ownerProxy.SetInterests("bowling, Go");
+            Console.WriteLine("Interests set from owner proxy");
+            try
+            {
+                ownerProxy.SetGeekRating(10);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Can't set rating from owner proxy");
+            }
+            Console.WriteLine("Rating is " + ownerProxy.GetGeekRating());
+
+            //Non Owner Proxy
+            IPerson nonOwnerProxy = GetNonOwnerProxy(joe);
+            Console.WriteLine("Name is " + nonOwnerProxy.GetName());
+            try
+            {
+                nonOwnerProxy.SetInterests("bowling, Go");
+            }
+            catch
+            {
+                Console.WriteLine("Can't set interests from non owner proxy");
+            }
+            nonOwnerProxy.SetGeekRating(3);
+            Console.WriteLine("Rating set from non owner proxy");
+            Console.WriteLine("Rating is " + nonOwnerProxy.GetGeekRating());
+        }
+    }
+}
---
+++ DesignPatterns/ProxyPatternDependencies/Classes.cs
@@ -0,0 +1,116 @@
+﻿using System.Reflection;
+
+namespace ProxyPatternDependencies
+{
+    public class Classes
+    {
+        public interface IPerson
+        {
+            string GetName();
+            string GetGender();
+            string GetInterests();
+            int GetGeekRating();
+            void SetName(string name);
+            void SetGender(string gender);
+            void SetInterests(string interests);
+            void SetGeekRating(int rating);
+        }
+
+        public class PersonImpl : IPerson
+        {
+            private string _name = String.Empty;
+            private string _gender = String.Empty;
+            private string _interests = String.Empty;
+            private int _rating;
+            private int _ratingCount;
+
+            public string GetName() => _name;
+            public string GetGender() => _gender;
+            public string GetInterests() => _interests;
+            public int GetGeekRating() => _ratingCount == 0 ? 0 : _rating/_ratingCount;
+            public void SetName(string name) => _name = name;
+            public void SetGender(string gender) => _gender = gender;
+            public void SetInterests(string interests) => _interests = interests;
+            public void SetGeekRating(int rating)
+            {
+                _rating += rating;
+                _ratingCount++;
+            }
+        }
+
+        public class OwnerInvocationHandler : DispatchProxy
+        {
+            private IPerson? _person;
+
+            public OwnerInvocationHandler() { }
+
+            public OwnerInvocationHandler(IPerson person)
+            {
+                _person = person;
+            }
+
+            public void SetPerson(IPerson person) => _person = person;
+
+            protected override object Invoke(MethodInfo targetMethod, object[] args)
+            {
+                try
+                {
+                    if (targetMethod.Name.Equals("SetGeekRating"))
+                    {
+                        throw new UnauthorizedAccessException("You are not permitted to set the Geek rating");
+                    }
+                    return targetMethod.Invoke(_person, args);
+                }
+                catch (TargetInvocationException ex)
+                {
+                    throw ex.InnerException ?? ex;
+                }
+            }
+        }
+
+        public class NonOwnerInvocationHandler : DispatchProxy
+        {
+            private IPerson? _person;
+
+            public NonOwnerInvocationHandler() { }
+
+            public NonOwnerInvocationHandler(IPerson person)
+            {
+                _person = person;
+            }
+
+            public void SetPerson(IPerson person) => _person = person;
+
+            protected override object Invoke(MethodInfo targetMethod, object[] args)
+            {
+                try
+                {
+                    if (!targetMethod.Name.Equals("SetGeekRating") && targetMethod.Name.StartsWith("Set"))
+                    {
+                        throw new UnauthorizedAccessException("You are not permitted to perform this operation");
+                    }
+                    return targetMethod.Invoke(_person, args);
+                }
+                catch (TargetInvocationException ex)
+                {
+                    throw ex.InnerException ?? ex;
+                }
+            }
+        }
+
+        public static IPerson GetOwnerProxy(IPerson person)
+        {
+            var proxy = DispatchProxy.Create<IPerson, OwnerInvocationHandler>();
+            ((OwnerInvocationHandler)(object)proxy).SetPerson(person);
+            return proxy;
+        }
+
+        public static IPerson GetNonOwnerProxy(IPerson person)
+        {
+            var proxy = DispatchProxy.Create<IPerson, NonOwnerInvocationHandler>();
+            ((NonOwnerInvocationHandler)(object)proxy).SetPerson(person);
+            return proxy;
+        }
+
+    }
+}
