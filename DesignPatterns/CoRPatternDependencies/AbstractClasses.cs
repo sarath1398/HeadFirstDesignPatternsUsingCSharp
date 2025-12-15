e518a0622d23353cfabdd3d259689b29bb8ba0b2
Sarath <sarath.u1398@gmail.com>
2025-12-16 01:42:20 +0530

    Added Chain of Responsibility pattern changes

---
+++ DesignPatterns/CoRPatternDependencies/AbstractClasses.cs
@@ -0,0 +1,19 @@
+﻿using static CoRPatternDependencies.Classes;
+
+namespace CoRPatternDependencies
+{
+    public class AbstractClasses
+    {
+        public abstract class Handler
+        {
+            protected Handler? successor;
+
+            public void SetSuccessor(Handler successor) => this.successor = successor;
+
+            public abstract void HandleRequest(Email email);
+
+            // Additional method deviating from the book to handle fallback scenario
+            public virtual void HandleFallback() => Console.WriteLine("Mail moved to snooze folder");
+        }
+    }
+}
---
+++ DesignPatterns/CoRPatternDependencies/Classes.cs
@@ -0,0 +1,91 @@
+﻿using static CoRPatternDependencies.AbstractClasses;
+
+namespace CoRPatternDependencies
+{
+    public class Classes
+    {
+        public class Email(string type, string message)
+        {
+            public string Type { get; } = type;
+            public string Message { get; } = message;
+        }
+
+        public class SpamHandler : Handler
+        {
+            public override void HandleRequest(Email email)
+            {
+                if (email.Type.Equals("SPAM", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Spam Handler - Deleting the spam email");
+                }
+                else if (successor != null)
+                {
+                    successor.HandleRequest(email);
+                }
+                else
+                {
+                    HandleFallback();
+                }
+            }
+        }
+
+        public class FanHandler : Handler
+        {
+            public override void HandleRequest(Email email)
+            {
+                if (email.Type.Equals("FAN", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Fan Handler - Forwarding fan mail to the CEO.");
+                }
+                else if (successor != null)
+                {
+                    successor.HandleRequest(email);
+                }
+                else
+                {
+                    HandleFallback();
+                }
+            }
+        }
+
+        public class ComplaintHandler : Handler
+        {
+            public override void HandleRequest(Email email)
+            {
+                if (email.Type.Equals("COMPLAINT", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Complaint Handler - Forwarding complaint to the Legal Department.");
+                }
+                else if (successor != null)
+                {
+                    successor.HandleRequest(email);
+                }
+                else
+                {
+                    HandleFallback();
+                }
+            }
+        }
+
+        public class NewLocHandler : Handler
+        {
+            public override void HandleRequest(Email email)
+            {
+                if (email.Type.Equals("NewLoc", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("New Location Handler - Forwarding new location requests" +
+                        " to the business development team");
+                }
+                else if (successor != null)
+                {
+                    successor.HandleRequest(email);
+                }
+                else
+                {
+                    HandleFallback();
+                }
+            }
+        }
+    }
+    
+}
