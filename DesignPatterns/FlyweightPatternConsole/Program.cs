e5f1ce856240bd1d39824a974f006d775b7feb45
Sarath <sarath.u1398@gmail.com>
2025-12-19 01:38:36 +0530

    Added flyweight pattern changes

---
+++ DesignPatterns/FlyweightPatternConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using static FlyweightPatternDependencies.Classes;
+
+namespace FlyweightPatternConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] treeData =
+            [
+                [10, 20, 5],
+                [50, 60, 1],
+                [100, 100, 12],
+                [120, 140, 8]
+            ];
+
+            TreeManager treeManager = new();
+
+            foreach (var tree in treeData)
+            {
+                treeManager.Add(tree[0], tree[1], tree[2]);
+            }
+
+            // Render all trees using the single Flyweight instance
+            treeManager.DisplayTrees();
+        }
+    }
+}
---
+++ DesignPatterns/FlyweightPatternDependencies/Classes.cs
@@ -0,0 +1,43 @@
+﻿namespace FlyweightPatternDependencies
+{
+    public class Classes
+    {
+        public class Tree
+        {
+            public static void Display(int age, int xCoord, int yCoord)
+            {
+                Console.WriteLine($"Displaying a tree of Age {age} at position ({xCoord},{yCoord})");
+            }
+        }
+
+        public class TreeManager
+        {
+            private Tree _tree;
+            private int _counter = 0;
+
+            // The cache for storing X-Axis Coordinates, Y-Axis Coordinates and Tree's Age
+            
+            // It is assumed that the X-Axis and Y-Axis coordinates are unique since multiple trees
+            // cannot be rendered in same page. 
+            private readonly Dictionary<int, List<int>> _cache = [];
+
+            public void Add(int xCoord, int yCoord, int age)
+            {
+                if (_cache.TryAdd(_counter++, [xCoord, yCoord, age]) == false)
+                {
+                    throw new ArgumentException(); // TODO: ideal approach is to create a lock object to avoid concurrent handling of objects
+                }
+            }
+
+            public void DisplayTrees()
+            {
+                foreach (var key in _cache.Keys)
+                {
+                    List<int> value = _cache[key];
+                    (int xCoord, int yCoord, int age) = (value[0], value[1], value[2]);
+                    Tree.Display(age, xCoord, yCoord);
+                }
+            }
+        }
+    }
+}
