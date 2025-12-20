92a02db1bf2e30f0d7f8628ad96c89e1081e5ea8
Sarath <sarath.u1398@gmail.com>
2025-12-21 00:27:51 +0530

    Added Memento Pattern Changes

---
+++ DesignPatterns/MementoPatternDependencies/Classes.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Channels;
+
+namespace MementoPatternDependencies
+{
+    public class Classes
+    {
+        public class GameMemento(string state)
+        {
+            public string SavedGameState { get; private set; } = state;
+        }
+
+        public class MasterGameObject
+        {
+            private string _gameState;
+
+            public void SetGameState(string level)
+            {
+                _gameState = level;
+                Console.WriteLine($"Congratulations!\n You advance to {_gameState}");
+            }
+
+            // Object return type ensures that the Memento is not accessible by the client
+            public object GetCurrentState()
+            {
+                return new GameMemento(_gameState);
+            }
+
+            public void RestoreState(object savedState)
+            {
+                if (savedState is GameMemento memento)
+                {
+                    _gameState = memento.SavedGameState;
+                    Console.WriteLine($"Last Checkpoint restored. You are at {_gameState}");
+                }
+            }
+        }
+    }
+}
---
+++ DesignPatterns/RPGGameClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using static MementoPatternDependencies.Classes;
+
+namespace RPGGameClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MasterGameObject gameObject = new();
+
+            gameObject.SetGameState("Level 12");
+
+            Console.WriteLine("Mission Accomplished!");
+
+            gameObject.SetGameState("Level 13");
+
+            object checkpoint = gameObject.GetCurrentState();
+
+            Console.WriteLine("Mission Failed. You are taken in Ambulance and you lost $100");
+
+            gameObject.RestoreState(checkpoint);
+
+        }
+    }
+}
